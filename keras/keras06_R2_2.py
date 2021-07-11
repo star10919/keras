099954f1767ceb2c9e06ebcfde1b4fedff4ac1e5
star10919 <star10919@naver.com>
2021-07-11 18:33:28 +0900

    21-07-11

--- keras/keras06_R2_2.py
+++ keras/keras06_R2_2.py
@@ -12,22 +12,22 @@ y = np.array([1, 2, 4, 3, 5])
 model = Sequential()
 model.add(Dense(6, input_dim=1))
 model.add(Dense(5))
-model.add(Dense(4))
-model.add(Dense(6))
-model.add(Dense(6))
-model.add(Dense(4))
+model.add(Dense(3))
+model.add(Dense(5))
+model.add(Dense(3))
+model.add(Dense(5))
 model.add(Dense(1))
 
 #3.컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
-model.fit(x, y, epochs=5500, batch_size=20000)
+model.fit(x, y, epochs=2500, batch_size=20000)
 
 #4.평가, 예측
 loss = model.evaluate(x, y)
 print('loss:', loss)
 
-y_predict = model.predict([6])
-print('6의 예측값 :', y_predict)
+# y_predict = model.predict([6])
+# print('6의 예측값 :', y_predict)
 
 '''
 #5. 결과값
