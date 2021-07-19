537616c19325372afee37973746021516f96e2d5
star10919 <star10919@naver.com>
2021-07-19 16:24:08 +0900

    21-07-19

--- keras/keras32_1_mnist_dnn.py
+++ keras/keras32_1_mnist_dnn.py
@@ -1,3 +1,4 @@
+from re import S
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
@@ -5,7 +6,7 @@ from icecream import ic
 
 ### dnn(Dense, 2차원)이 cnn(Convolution, 4차원) 보다 연산량이 훨씬 적음
 # DNN 구해서 CNN 과 비교
-# DNN + GlobalAveragePooling 구해서 CNN 비교
+# CNN + GlobalAveragePooling 구해서 CNN 과 비교
 
 # 1. 데이터
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -20,7 +21,13 @@ ic(np.unique(y_train))   # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 y_train = y_train.reshape(-1,1)
 y_test = y_test.reshape(-1,1)
 
-# #1-2. 데이터 전처리
+# 1-2. x 데이터 전처리
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+# 1-3. y 데이터 전처리
 from sklearn.preprocessing import OneHotEncoder
 one = OneHotEncoder()
 one.fit(y_train)
@@ -31,17 +38,30 @@ y_test = one.transform(y_test).toarray() # (10000, 10)
 
 # 2. 모델 구성
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, GlobalAveragePooling2D, Dropout, GlobalAveragePooling1D
 
 model = Sequential()
-# model.add(Conv2D(filters=32, kernel_size=(4,4), padding='same', input_shape=(28, 28, 1)))
-model.add(Dense(100, input_shape=(28 * 28,)))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
+# dnn
+# model.add(Dense(100, input_shape=(28 * 28,)))
+# model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(128, activation='relu'))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dense(10, activation='softmax'))
 
+# cnn
+model.add(Conv2D(filters=100, kernel_size=(4,4), padding='same', input_shape=(28, 28, 1)))
+model.add(Conv2D(128, (2, 2), padding='valid', activation='relu'))
+model.add(Conv2D(128, (2, 2), padding='valid', activation='relu'))
+model.add(Conv2D(64, (2, 2), padding='valid', activation='relu'))
+model.add(Conv2D(64, (2, 2), padding='valid', activation='relu'))
+model.add(Conv2D(32, (2, 2), padding='valid', activation='relu'))
+model.add(Flatten())
+model.add(GlobalAveragePooling1D())
 model.add(Dense(10, activation='softmax'))
-
 model.summary()
 
 
@@ -66,4 +86,15 @@ print('accruacy :', results[1])
 '''
 category : 0.04307371377944946
 accruacy : 0.9900000095367432
+
+
+dnn
+category : 0.10670371353626251
+accruacy : 0.9728999733924866
+
+cnn
+
+
+cnn + GlobalAveragePooling
+
 '''
