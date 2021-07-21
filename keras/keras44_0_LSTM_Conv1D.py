efd8b7389832bc042d8436991225ad48eece2f45
star10919 <star10919@naver.com>
2021-07-21 13:31:20 +0900

    21-07-21

--- keras/keras44_0_LSTM_Conv1D.py
+++ keras/keras44_0_LSTM_Conv1D.py
@@ -2,6 +2,7 @@ from re import S
 import numpy as np
 from icecream import ic
 from sklearn.utils import shuffle
+from tensorflow.python.keras.layers.core import Flatten
 
 '''
 #실습 1~100까지의 데이터를
@@ -69,7 +70,7 @@ x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 x_val = scaler.transform(x_val)
 x_pred = scaler.transform(x_pred)
-ic(x_train.shape, x_val.shape, x_test.shape)   # x_train.shape: (9, 5), x_val.shape: (1, 5), x_test.shape: (10, 5)
+ic(x_train.shape, x_val.shape)   # x_train.shape: (9, 5), x_val.shape: (1, 5), x_test.shape: (10, 5)
 
 
 x_train = x_train.reshape(9, 5, 1)
@@ -77,12 +78,15 @@ x_val = x_val.reshape(1, 5, 1)
 x_test = x_test.reshape(10, 5, 1)
 x_pred = x_pred.reshape(4, 5, 1)
 
+
 # 2. 모델 구성
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dense, Conv1D
 
 model = Sequential()
-model.add(LSTM(units=10, activation='relu', input_shape=(5, 1)))
+# model.add(LSTM(units=10, activation='relu', input_shape=(5, 1)))
+model.add(Conv1D(10, 2, input_shape=(5, 1)))
+model.add(Flatten())
 model.add(Dense(64, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
@@ -92,6 +96,7 @@ model.add(Dense(8, activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1))
 
+model.summary()
 
 # 3. 컴파일(ES), 훈련
 model.compile(loss='mse', optimizer='adam')
@@ -130,4 +135,10 @@ ic(rmse)
 loss : 0.5054280757904053
 R2 스코어 :  0.8096759070991538
 ic| rmse: 0.4877551805220091
+
+*Conv1D
+걸린시간 : 12.419901132583618
+loss : 1.1263048648834229
+R2 스코어 :  -233.0424494003295
+ic| rmse: 17.10418258059741
 '''
