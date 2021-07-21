80ce135f749f259ef5803aef98d9693de9908cc5
star10919 <star10919@naver.com>
2021-07-21 15:29:59 +0900

    21-07-21

---
+++ keras/keras45_2_load_model.py
@@ -0,0 +1,137 @@
+import numpy as np
+from tensorflow.keras.datasets import cifar100, mnist
+from icecream import ic
+
+### save한 모델 가져오기
+
+
+# 1. 데이터
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+ic(x_train.shape, y_train.shape)   
+ic(x_test.shape, y_test.shape)     
+
+# 1-2. x 데이터 전처리 - scaler:2차원에서만 가능
+x_train = x_train.reshape(60000, 28 * 28)   # 4차원 -> 2차원
+x_test = x_test.reshape(10000, 28 * 28)
+# 전처리 하기 -> scailing
+print(x_train.shape, x_test.shape) # (50000, 3072)-2차원, (10000, 3072)-2차원
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, PowerTransformer, QuantileTransformer
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)   # fit_transform 은  x_train 에서만 사용한다!!!!!!!!!!!!
+x_test = scaler.transform(x_test)
+
+x_train = x_train.reshape(60000, 28, 28, 1)    # 4차원으로 다시 reshape(Conv2d 사용해야 되니까)
+x_test = x_test.reshape(10000, 28, 28, 1)
+
+# 1-3. y 데이터 전처리 -> one-hot-encoding
+ic(np.unique(y_train))    # 100개
+# from tensorflow.keras.utils import to_categorical   # 0,1,2 값이 없어도 무조건 생성/shape유연
+# y_train = to_categorical(y_train)
+# y_test = to_categorical(y_test)
+# print(y_train.shape, y_test.shape)
+
+from sklearn.preprocessing import OneHotEncoder    # sklearn으로 되어 있는 애들은 모두 2차원으로 해줘야 함/OneHotEncoder는 무조건 2차원으로 해줘야 함
+y_train = y_train.reshape(-1,1)
+y_test = y_test.reshape(-1,1)
+
+one = OneHotEncoder()
+# one.fit(y_train)
+y_train = one.fit_transform(y_train).toarray()   # (50000, 100)
+y_test = one.transform(y_test).toarray()     # (10000, 100)
+# ic(y_train.shape, y_test.shape)
+
+
+# 2. 모델 구성(GlobalAveragePooling2D 사용) - load model로 대체
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPool2D, Dropout, GlobalAveragePooling2D
+
+# model = Sequential()
+# model.add(Conv2D(128, kernel_size=(2, 2), 
+#                     padding='valid', input_shape=(28, 28, 1), activation='relu'))
+# # model.add(Dropout(0, 2)) # 20% node Dropout
+# model.add(Dropout(0.2))
+# model.add(Conv2D(128, (2,2), padding='same', activation='relu'))   
+# model.add(MaxPool2D()) 
+# model.add(Conv2D(128, (2,2),padding='valid', activation='relu'))  
+# model.add(Dropout(0.2))
+# model.add(Conv2D(128, (2,2), padding='same', activation='relu')) 
+# model.add(MaxPool2D()) 
+# model.add(Conv2D(64, (2,2), padding='valid', activation='relu')) 
+# model.add(Dropout(0.2))
+# model.add(Conv2D(64, (2,2), padding='same', activation='relu')) 
+# model.add(MaxPool2D()) 
+# model.add(Flatten()) 
+# model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dense(10, activation='softmax'))
+
+model = load_model('./_save/keras45_1_save_model.h5')
+
+model.summary()
+
+# model.save('./_save/keras45_1_save_model.h5')   # 저장되는 확장자 : h5   # ./ : 현재위치(STUDY 폴더)
+
+
+# 3. 컴파일(ES), 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode='min')
+
+import time 
+start_time = time.time()
+hist = model.fit(x_train, y_train, epochs=10, verbose=1, callbacks=[es], validation_split=0.2, shuffle=True, batch_size=100)
+end_time = time.time() - start_time
+
+
+# 4. 평가, 예측
+results = model.evaluate(x_test, y_test)
+print("걸린시간 :", end_time)
+print('category :', results[0])
+print('accuracy :', results[1])
+
+
+# # 시각화 
+# import matplotlib.pyplot as plt
+# plt.figure(figsize=(9,5))
+
+# # 1
+# plt.subplot(2, 1, 1) # 2개의 플롯을 할건데, 1행 1열을 사용하겠다는 의미 
+# plt.plot(hist.history['loss'], marker='.', c='red', label='loss')
+# plt.plot(hist.history['val_loss'], marker='.', c='blue', label='val_loss')
+# plt.grid()
+# plt.title('loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(loc='upper right')
+
+# # 2
+# plt.subplot(2, 1, 2) # 2개의 플롯을 할건데, 1행 2열을 사용하겠다는 의미 
+# plt.plot(hist.history['acc'])
+# plt.plot(hist.history['val_acc'])
+# plt.grid()
+# plt.title('acc')
+# plt.ylabel('acc')
+# plt.xlabel('epoch')
+# plt.legend(['acc', 'val_acc'])
+
+# plt.show()
+
+
+'''
+* 모델 작성
+걸린시간 : 98.12368416786194
+category : 0.03186216577887535
+accuracy : 0.991599977016449
+
+* load_model로 실행  -  keras45_1_save_model.h5
+(모델만 저장해서 돌린거라 돌릴 때마다 다르게 나옴./ 가중치가 들어가면 똑같이 나옴)
+걸린시간 : 98.74596667289734
+category : 0.0277412012219429
+accuracy : 0.9921000003814697
+'''
---
+++ keras/keras46_2_load_model.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_diabetes
+from icecream import ic
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+# save 파일명 : keras46_1_save_model.h5
+
+#1. 데이터
+datasets = load_diabetes()
+
+x = datasets.data
+y = datasets.target
+
+ic(x.shape, y.shape)  # (442, 10)  (442,)
+
+ic(datasets.feature_names)   
+#['age', 'sex', 'bmi', 'bp', 's1', 's2', 's3', 's4', 's5', 's6']
+ic(datasets.DESCR)
+
+ic(x[:30])
+ic(np.min(y), np.max(y))
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, shuffle=True, random_state=9)
+x_train, x_val, y_train, y_val = train_test_split(x_test, y_test, train_size=0.8, shuffle=True, random_state=9)
+
+#1-2. x 데이터 전처리
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+x_val = scaler.transform(x_val)
+
+
+#2. 모델구성(validation)
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense
+# model = Sequential()
+# model.add(Dense(128, input_shape=(10,), activation='relu'))  #relu : 음수값은 0, 양수만 제대로 잡음
+# # model.add(Dense(64, activation='relu'))
+# model.add(Dense(32, activation='relu'))
+# model.add(Dense(64, activation='relu'))
+# # model.add(Dense(32, activation='relu'))
+# model.add(Dense(16, activation='relu'))
+# model.add(Dense(8, activation='relu'))
+# model.add(Dense(4, activation='relu'))
+# model.add(Dense(1))
+
+model = load_model('./_save/keras46_1_save_model.h5')
+
+model.summary()
+
+# model.save('./_save/keras46_1_save_model.h5')
+
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+
+import time
+start = time.time()
+model.fit(x_train, y_train, epochs=100, batch_size=10, shuffle=True, verbose=0)
+end = time.time() - start
+
+
+#4. 평가, 예측(mse, r2)
+loss = model.evaluate(x_test, y_test)
+print("걸린시간 :", end)
+ic(loss)
+
+y_predict = model.predict(x_test)
+r2 = r2_score(y_test, y_predict)
+ic(r2)
+
+
+# 과제 0.62까지 올려라!
+'''
+결과값
+ic| loss: 2092.216552734375
+ic| r2: 0.6214054692239842
+
+*x 전처리
+걸린시간 : 3.319180727005005
+ic| loss: 1291.806396484375
+ic| r2: 0.7662427290354967
+
+*load model
+걸린시간 : 3.487668037414551
+ic| loss: 1371.6661376953125
+ic| r2: 0.7517918267211269
+'''
