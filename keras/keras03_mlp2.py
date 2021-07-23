88f6c954e324fcef481fb1194230fc058f3b33ac
star10919 <star10919@naver.com>
2021-07-23 17:43:43 +0900

    21-07-23

--- keras/keras03_mlp2.py
+++ keras/keras03_mlp2.py
@@ -18,6 +18,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import numpy as np
 import matplotlib.pyplot as plt
+from icecream import ic
 
 #1-1. 데이터
 x = np.array([[1,2,3,4,5,6,7,8,9,10], [1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.5, 1.4, 1.3],[10,9,8,7,6,5,4,3,2,1]])   # (3,10)
