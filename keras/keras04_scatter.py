634840b4e96b1e45c932e3d86f0638f96530320b
star10919 <star10919@naver.com>
2021-07-23 17:46:40 +0900

    21-07-23

--- keras/keras04_scatter.py
+++ keras/keras04_scatter.py
@@ -2,6 +2,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 import numpy as np
 import matplotlib.pyplot as plt
+from icecream import ic
 
 #1. 데이터
 x = np.array([1,2,3,4,5,6,7,8,9,10])   # 10스칼라(=1벡터)
